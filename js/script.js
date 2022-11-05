e6a37053b6cd10548b062577fd713067a13d2ac7
NatalyVas <113516622+NatalyVas@users.noreply.github.com>
2022-11-05 19:39:26 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(".open-popup").click(function(e){
+    e.preventDefault();
+    $(".popup").fadeIn(300,function(){$(this).focus();});
+});
+
+$('.close').click(function() {
+   $(".popup").fadeOut(300);
+});
+
+$(".popup").on('blur',function(){
+    $(this).fadeOut(300);
+});
